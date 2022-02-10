333f2eeaf3dbb6ebfc589ebf15e43d9232be83a7
AnaStadelhofer <87942437+AnaStadelhofer@users.noreply.github.com>
2022-02-10 15:19:03 -0300

    Alterações no Capybara

---
+++ PrimeirosEstudos/Udemy/Capybara/test/features/step_definitions/formulario.rb
@@ -0,0 +1,17 @@
+Dado('que o usuário já esteja logado no sistema') do
+    visit ('fusion/portal/action/Login/view/normal')
+    find('#user').set "ana.stadelhofer"
+    find('#pass').set "Neo#2022"
+    click_on('Entrar')
+end
+
+Quando('o usuário acessar o registros do formulário') do
+    find('#menu_item_small_40').click
+    find(class: 'icon-three-files').click
+    sleep(5)
+    find(class: 'pull-left').check
+end
+
+Então('ele pode criar novos registros') do
+
+end
