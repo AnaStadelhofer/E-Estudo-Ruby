3f943c35acc53081a37a79cbf379ec6962f5b604
AnaStadelhofer <87942437+AnaStadelhofer@users.noreply.github.com>
2022-03-04 14:16:53 -0300

    Preenchendo campos em cenário simples

--- PrimeirosEstudos/Udemy/Relatorio/features/pages/cadastrar_usuario_page.rb
+++ PrimeirosEstudos/Udemy/Relatorio/features/pages/cadastrar_usuario_page.rb
@@ -1,4 +1,58 @@
 class CadastrarUsuarioObj < SitePrism::Page
     set_url 'users/new'
     
+    set_url 'users/new'
+
+    element :nome, '#user_name'
+    element :sobrenome, '#user_lastname'
+    element :email, '#user_email'
+    element :endereco, '#user_address'
+    element :universidade, '#user_university'
+    element :profissao, '#user_profile'
+    element :genero, '#user_gender'
+    element :idade, '#user_age'
+
+    def cadastrar_todos(nome_preenchido, sobrenome_preenchido, email_preenchido, endereco_preenchido, universidade_preenchido, profissao_preenchido, genero_preenchido, idade_preenchido)
+        nome.set nome_preenchido
+        sobrenome.set sobrenome_preenchido
+        email.set email_preenchido
+        endereco.set endereco_preenchido
+        universidade.set universidade_preenchido
+        profissao.set profissao_preenchido
+        genero.set genero_preenchido
+        idade.set idade_preenchido
+    end
+
+    def preencher_nome(nome_preenchido)
+        nome.set nome_preenchido
+    end
+
+    def preencher_sobrenome(sobrenome_preenchido)
+        sobrenome.set sobrenome_preenchido
+    end
+
+    def preencher_email(email_preenchido)
+        email.set email_preenchido
+    end
+
+    def preencher_endereco(endereco_preenchido)
+        endereco.set endereco_preenchido
+    end
+
+    def preencher_universidade(universidade_preenchido)
+        universidade.set universidade_preenchido
+    end
+
+    def preencher_profissao(profissao_preenchido)
+        profissao.set profissao_preenchido
+    end
+
+    def preencher_genero(genero_preenchido)
+        genero.set genero_preenchido
+    end
+
+    def preencher_idade(idade_preenchido)
+        idade.set idade_preenchido
+    end
+
 end
--- PrimeirosEstudos/Udemy/Relatorio/features/step_definitions/cadastrar_usuario.rb
+++ PrimeirosEstudos/Udemy/Relatorio/features/step_definitions/cadastrar_usuario.rb
@@ -1,6 +1,8 @@
 Quando('o usuário informar os dados para realizar o cadastro') do
     cadastra_user.load
     sleep(2)
+    cadastra_user.cadastrar_todos('Nome', 'Sobrenome', 'Email.user@gmail.com', 'Endereco', 'Univwesidade', 'Medico', 'Homi', '25')
+    sleep(2)
 end
 
 Então('o usuário será cadastrado') do
