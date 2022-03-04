190bc2362fd613407ec9a9bb998daa8079ad92bb
AnaStadelhofer <87942437+AnaStadelhofer@users.noreply.github.com>
2022-03-04 16:09:46 -0300

    Gerando Relatório

--- PrimeirosEstudos/Udemy/Relatorio/features/support/env.rb
+++ PrimeirosEstudos/Udemy/Relatorio/features/support/env.rb
@@ -3,17 +3,25 @@ require 'site_prism'
 require 'selenium-webdriver'
 require 'ostruct'
 require_relative 'page_helper.rb'
+require_relative 'helper.rb'
 
 World(Pages)
+World(Helper)
 
 AMBIENTE = ENV['AMBIENTE']
-
 BROWSER = ENV['BROWSER']
 
 Capybara.register_driver :selenium do |app|
 
     if BROWSER.eql?('chrome')
         Capybara::Selenium::Driver.new(app, :browser => :chrome)
+
+    elsif BROWSER.eql?('chrome_headless')
+        Capybara::Selenium::Driver.new(app, :browser => :chrome,
+        desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+            'chromeOptions' => {'args' =>['--headless', 'disable-gpu']}
+        )
+        )
     elsif BROWSER.eql?('firefox')
         Capybara::Selenium::Driver.new(app, :browser => :firefox, :marionette => true)
     elsif BROWSER.eql?('ie')
