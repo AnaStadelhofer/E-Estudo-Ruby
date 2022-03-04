04c192f9fbf37f67fa09f18c95671180e4f549c6
AnaStadelhofer <87942437+AnaStadelhofer@users.noreply.github.com>
2022-03-04 14:25:46 -0300

    Configurando outros browsers

--- PrimeirosEstudos/Udemy/Relatorio/features/support/env.rb
+++ PrimeirosEstudos/Udemy/Relatorio/features/support/env.rb
@@ -8,11 +8,26 @@ World(Pages)
 
 AMBIENTE = ENV['AMBIENTE']
 
+BROWSER = ENV['BROWSER']
+
+Capybara.register_driver :selenium do |app|
+
+    if BROWSER.eql?('chrome')
+        Capybara::Selenium::Driver.new(app, :browser => :chrome)
+    elsif BROWSER.eql?('firefox')
+        Capybara::Selenium::Driver.new(app, :browser => :firefox, :marionette => true)
+    elsif BROWSER.eql?('ie')
+        Capybara::Selenium::Driver.new(app, :browser => :internet_explorer)
+    elsif BROWSER.eql?('safari')
+        Capybara::Selenium::Driver.new(app, :browser => :safari)
+    end
+end
+
 CONFIG = YAML.load_file(File.dirname(__FILE__) + "/ambientes/#{AMBIENTE}.yml")
 
 Capybara.configure do |config|
     #selenium   selenium_chrome selenium_chrome_healess
-    config.default_driver = :selenium_chrome
+    config.default_driver = :selenium
     config.app_host = CONFIG['url_padrao']
     config.default_max_wait_time = 5
 end
